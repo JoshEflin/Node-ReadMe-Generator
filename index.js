95916748bd22b6fc65978df904dd8b833da948ed
ragingpenguin <eflinjh@gmail.com>
2023-01-26 12:31:50 -0500

    installed inquirer

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
-
+const generate = require ('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['cat', 'dog'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +11,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+generate(questions)
