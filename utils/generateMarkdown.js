a7e99b0007979788035ca432c15cb60d9269af14
ragingpenguin <eflinjh@gmail.com>
2023-01-30 13:00:27 -0500

    fixed badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,7 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== "none (no license)"){
-    let badge =license.split ('').join('_');
+    let badge =license.split(' ').join('_');
     return `![License](https://img.shields.io/badge/license-${badge}-blue?logo=github)`
   } else {
     return "";
